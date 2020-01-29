ce877ac4fb31725a72c019cdee158432a81d8c29
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-01-29 16:05:58 +0000

    Done.

---
+++ learning-if-end.rb
@@ -0,0 +1,18 @@
+# run_code_inside = false 
+# puts "Code before if...end"
+
+# if run_code_inside
+#   puts "code inside"
+# end
+
+# puts "Code after if...end"
+
+chance_of_rain = 1
+
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the fine day!"
+end
+
+puts "Oh, and always wear sunscreen!"
